ede67e1ca571877b1b032ebc17c9fec906ab98b8
etwyniel <aymeric.beringer@epita.fr>
2016-11-17 23:54:52 +0100

    Added comment printing with indentation.

--- HackerNews/Program.cs
+++ HackerNews/Program.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Net;
 using System.IO;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Threading;
 using System.Runtime.Serialization.Json;
 using System.Web.Script.Serialization;
@@ -21,6 +23,8 @@ namespace HackerNews
 
 	class MainClass
 	{
+		//public static StringWriter baseWriter = new StringWriter();
+		public static IndentedTextWriter writer = new IndentedTextWriter (Console.Out, "    ");
 
 		public static List<string> getX(string x, int limit)
 		{
@@ -80,17 +84,16 @@ namespace HackerNews
 
 		public static List<Dictionary<string, dynamic>> getKids (string id, int limit = -1)
 		{
-			List<Dictionary<string, dynamic>> output = new List<Dictionary<string, dynamic>>();
-			List<Thread> threads = new List<Thread>();
+			List<Dictionary<string, dynamic>> output = new List<Dictionary<string, dynamic>> ();
+			List<Thread> threads = new List<Thread> ();
 			Dictionary<string, dynamic> item = getItem (id);
-			int kids = item ["kids"].Count;
+			int kids = (item.ContainsKey("kids")) ? item ["kids"].Count : 0;
 			if (kids == 0) {
 				return output;
 			} else {
 				foreach (int i in Enumerable.Range(0,
-					(limit == -1) ? kids : Math.Min(limit, kids)))
-				{
-					threads.Add(new Thread(() => output.Add(getItem(item["kids"][i].ToString()))));
+					(limit == -1) ? kids : Math.Min(limit, kids))) {
+					threads.Add (new Thread (() => output.Add (getItem (item ["kids"] [i].ToString ()))));
 					threads [i].Start ();
 				}
 				bool running = true;
@@ -102,23 +105,97 @@ namespace HackerNews
 							break;
 						}
 					}
-					Thread.Sleep (5);
+					Thread.Sleep (20);
 				}
 				return output;
 			}
 		}
 
-		public void addComment(List<Dictionary<string, dynamic>> list, string id)
+		public static void printComments(List<Dictionary<string, dynamic>> comments,int maxDepth = -1, int curDepth = 0) 
 		{
-			list.Add (getItem (id));
+			HtmlToText h = new HtmlToText ();
+			foreach (Dictionary<string, dynamic> c in comments) {
+				/*string kids = "";
+				try {
+					kids = c ["kids"].Count.ToString () + " replies.\n";
+				} catch {}*/
+				int kids;
+				try {
+					kids = c["kids"].Count;
+				} catch {
+					kids = 0;
+				}
+				if (!c.ContainsKey("deleted")) {
+					writer.WriteLine (c ["by"] + ":");
+					string s = indent(h.ConvertHtml(c["text"]), writer.Indent);
+					s = s.Remove (s.Length - 1);
+					Console.WriteLine(s);
+					//writer.WriteLine (h.ConvertHtml (c ["text"]));
+				} else {
+					writer.WriteLine ("[deleted]:");
+					writer.WriteLine ("[deleted]");
+				}
+				if (kids > 0 && (curDepth < maxDepth || maxDepth == -1)) {
+					writer.WriteLine ();
+					writer.Indent += 1;
+					printComments (getKids(c["id"].ToString()), maxDepth, curDepth + 1);
+					writer.Indent -= 1;
+				}
+				Console.WriteLine ();
+				//Console.WriteLine (c ["by"] + ":\n" + h.ConvertHtml (c ["text"]) + "\n" + kids);
+			}
+		}
+
+		public static string indent (string s, int n, int baseIndent = 4) {
+			string r = "";
+			if (s.Contains ('\n')) {
+				foreach (string sub in s.Split ('\n')) {
+					r += indent (sub, n, baseIndent);
+				}
+				return r;
+			} else {
+				int length = Console.BufferWidth - n * baseIndent;
+				string ind = new String (' ', baseIndent * n);
+				while (s != "" && s != ".") {
+					
+					//WIP - Trying
+					int index = (s.Length <= length) ?
+						s.Length - 1 : Math.Max(s.LastIndexOf (' ', length - 1), s.LastIndexOf('.', length - 1));
+					r += ind + s.Substring (0, index + 1) + "\n";
+					//Console.WriteLine (s.Substring (0, index + 1));
+					s = s.Remove (0, index + 1);
+					/*
+					r += ind + s.Substring (0, Math.Min (length, s.Length)) + "\n";
+					s = s.Remove (0, Math.Min (length, s.Length));*/
+				}
+				//r = r.Remove (r.Length - 1);
+				return r;
+			}
 		}
 
 		public static void Main (string[] args)
 		{
+			writer.Indent = 0;
 			HtmlToText h = new HtmlToText ();
-			List<Dictionary<string, dynamic>> item = getKids (getBest () [0]);
-			Console.WriteLine (item.Count.ToString() + " comments.");
-			Console.WriteLine ("Best story: " + h.ConvertHtml (item [0] ["text"]) + " - by " + item [0] ["by"] + "\n");// + 
+			Dictionary<string, dynamic> best = getItem(getBest () [0]);
+			string story = best["title"] + "\n";
+			try {
+				story += h.ConvertHtml(best["text"]);
+			} catch {
+				story += best ["url"];
+			}
+			Console.WriteLine ("Best story:\n" + story + " - by " + best ["by"] + "\n\n");
+			List<Dictionary<string, dynamic>> comments = getKids (best ["id"].ToString());
+			Console.WriteLine (comments.Count.ToString() + " comments.");/*
+			foreach (Dictionary<string, dynamic> c in comments) {
+				string kids = "";
+				try {
+					kids = c["kids"].Count.ToString() + " replies.\n";
+				} catch {};
+				Console.WriteLine (c ["by"] + ":\n" + h.ConvertHtml (c ["text"]) + "\n" + kids);
+			}*/
+			printComments (comments);
+			//Console.WriteLine ("Best story: " + h.ConvertHtml (item [0] ["text"]) + " - by " + item [0] ["by"] + "\n");// + 
 				//((item[0]["descendants"] > 0) ? item[0]["kids"].Count.ToString() : "0") + " replies.");//+ item["url"]);
 		}
 	}
